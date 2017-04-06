8db5b42c6c915d5dd0178a9279a1db12fe83ec99
jessehartloff <hartloff@buffalo.edu>
2017-04-06 17:49:39 -0400

    initial commit

---
+++ src/NetworkingLecture3.java
@@ -0,0 +1,19 @@
+
+public class NetworkingLecture3 {
+
+
+    // 5 points
+    static String Q1(String tid) {
+        // Send an HTTP GET request to http://fury.cse.buffalo.edu/api/question with query string arguments
+        // of "lecture" and "tid" where the value of "lecture" is "3" and the value of "tid" is the value
+        // provided as the input to this method
+
+        return "";
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
